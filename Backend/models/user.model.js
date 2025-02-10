683337e4aeac4359d59c12d1572c34e021367bcd
Syed Faizaan Ahmad <syedashffaizaan1999@gmail.com>
2025-02-11 01:19:54 +0530

    Posts Routes

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -60,6 +60,14 @@ const userSchema = new mongoose.Schema({
         default: "",
     },
 
+    likedPosts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+            default: [],
+        },
+    ],
+
 }, {timestamps: true });
 
 const User = mongoose.model("User", userSchema);
