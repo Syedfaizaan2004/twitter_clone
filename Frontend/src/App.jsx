684c004a6a9a5d017188a7d708f485c8b640a87a
Syed Faizaan Ahmad <syedashffaizaan1999@gmail.com>
2025-02-18 22:49:54 +0530

    UI design

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import SignUpPage from './pages/auth/signup/SignUpPage'
+import LoginPage from './pages/auth/login/LoginPage'
+import Sidebar from './components/common/Sidebar'
+import RightPanel from './components/common/RightPanel'
+import NotificationPage from './pages/notification/NotificationPage'
+import ProfilePage from './pages/profile/ProfilePage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Syed Faizan Ahmad</h1>
-      <button class="bg-blue-500 ...">Button A</button>
+      <div className="flex max-w-6xl max-auto">
+        {/* common components, bc it is not wraped with route. */}
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/notifications" element={<NotificationPage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
+        </Routes>
+        <RightPanel />
+      </div>
     </>
   )
 }
